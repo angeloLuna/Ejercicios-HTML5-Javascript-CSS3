9ce55b61ac8c654d788b130506969368f238de4f
angeloLuna <angel.luna.najera@gmail.com>
2016-01-23 01:38:44 -0600

    un circulo dibujado con arc() en canvas

--- script.js
+++ script.js
@@ -2,16 +2,7 @@ function iniciar(){
 	var elemento=document.getElementById('lienzo');
 	lienzo=elemento.getContext('2d');
 	lienzo.beginPath();
-	lienzo.moveTo(100,100);
-	lienzo.lineTo(200,200);
-	lienzo.lineTo(100,200);
-	lienzo.clip();
-
-	lienzo.beginPath();
-	for(i=0; i<300; i=i+10){
-		lienzo.moveTo(0,i);
-		lienzo.lineTo(500, i);
-	}
+	lienzo.arc(100,100,50,0,Math.PI*2, false);
 	lienzo.stroke();
 }
 
