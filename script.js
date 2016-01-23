9fadd36d9d843def14d934710cf4bd9c7d005eeb
angeloLuna <angel.luna.najera@gmail.com>
2016-01-23 01:39:47 -0600

    sólo 90 grados de un circulo dibujado con arc() en canvas

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function iniciar(){
 	var elemento=document.getElementById('lienzo');
 	lienzo=elemento.getContext('2d');
 	lienzo.beginPath();
-	lienzo.arc(100,100,50,0,Math.PI*2, false);
+	lienzo.arc(100,100,50,0,Math.PI/180*90, false);
 	lienzo.stroke();
 }
 
